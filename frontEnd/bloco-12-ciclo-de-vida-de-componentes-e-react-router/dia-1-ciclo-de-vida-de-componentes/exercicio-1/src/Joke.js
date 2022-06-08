a085c0b16de1f617a3e3c8fc8cb2c28e71340bd0
Breno Nascimento <satsukebare23@gmail.com>
2022-06-08 18:33:40 -0300

    criando fetch piada

--- frontEnd/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercicio-1/src/Joke.js
+++ frontEnd/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercicio-1/src/Joke.js
@@ -1,20 +1,113 @@
-import React from 'react';
+import React, { cloneElement } from 'react';
 
 class Jokes extends React.Component {
   constructor() {
+    super()
     this.state = {
-      nowJoke: '',
-      jokeStorage: []
+      jokeObj: undefined,
+      loading: false,
+      storedJokes: [],
     }
+
+    this.fetchJoke = this.fetchJoke.bind(this);
+    this.addPiada = this.addPiada.bind(this);
+    this.saveJoke = this.saveJoke.bind(this);
+    this.getJokesInList = this.getJokesInList.bind(this)
+    this.clearJokes = this.clearJokes.bind(this);
+    this.deletAJoke = this.deletAJoke.bind(this);
+  }
+
+  async saveJoke() {
+    await localStorage.setItem("itemList", JSON.stringify(this.state.storedJokes))
+    console.log('salvou')
+  }
+
+    async fetchJoke()  {
+    const requestHeaders = { headers: { Accept: 'application/json' } }
+    const requestJoke = await fetch('https://icanhazdadjoke.com/', requestHeaders);
+    const jokeObj = await requestJoke.json();
+    this.setState({
+      jokeObj: jokeObj,
+    })
+  };
+
+  componentDidMount() {
+    this.fetchJoke();
+    this.getJokesInList();
+  };
+
+  getJokesInList() {
+    const getLocal = localStorage.getItem("itemList")
+    const response = JSON.parse(getLocal)
+    this.setState({
+      storedJokes: response
+    })
+  };
+
+  async addPiada() {
+    this.setState({
+      loading:true
+    })
+    const requestHeaders = { headers: { Accept: 'application/json' } }
+    const requestJoke = await fetch('https://icanhazdadjoke.com/', requestHeaders);
+    const jokeObj = await requestJoke.json();
+    const cloneNowState = this.state.storedJokes;
+    cloneNowState.push(jokeObj);
+    this.setState({
+      storedJokes: cloneNowState,
+    })
+    this.setState({
+      loading:false
+    })
+    this.saveJoke();
+  }
+
+  clearJokes() {
+    this.setState({
+      storedJokes: []
+    })
+    this.saveJoke();
+  }
+
+  deletAJoke(event) {
+    console.log(this.state.storedJokes);
+    const item = event.target.innerText
+    if(this.state.storedJokes.length === 1) {
+      this.setState({
+        storedJokes: []
+      })
+    localStorage.setItem("itemList", JSON.stringify([]))
+      return
+    }
+    const filtedList = this.state.storedJokes.filter((piada) =>
+      piada.joke !== item);
+    this.setState({
+      storedJokes: filtedList
+    })
+    localStorage.setItem("itemList", JSON.stringify(filtedList))
   }
 
   render() {
-const fetchJoke = async () => {
-  
-}
-
+    const LoadingElement = <span>Loading...</span>
     return (
-      <h1>dnwaoiu</h1>
+      <div>
+        {this.state.storedJokes.map(({ id, joke }) => (<p onClick={this.deletAJoke} key={id}>{joke}</p>))}
+        {
+          this.state.loading ? (
+            <p>
+              Loading...
+            </p>
+          ) : (
+            <div/>
+          )
+        }
+        <button onClick={this.addPiada}>
+          nova piada
+        </button>
+        <button onClick={this.clearJokes}>
+          Clear jokes
+        </button>
+      </div>
     );
   }
 }
