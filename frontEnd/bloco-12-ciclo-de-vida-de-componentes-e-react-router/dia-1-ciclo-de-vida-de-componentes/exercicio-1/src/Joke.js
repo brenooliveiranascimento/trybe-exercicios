4ed94289f740f178a6dec99c99fa3f42f1d105bb
Breno Nascimento <satsukebare23@gmail.com>
2022-06-08 16:26:25 -0300

    criando componente Jokes

---
+++ frontEnd/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercicio-1/src/Joke.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Jokes extends React.Component {
+  constructor() {
+    this.state = {
+      nowJoke: '',
+      jokeStorage: []
+    }
+  }
+
+  render() {
+const fetchJoke = async () => {
+  
+}
+
+    return (
+      <h1>dnwaoiu</h1>
+    );
+  }
+}
+export default Jokes;
