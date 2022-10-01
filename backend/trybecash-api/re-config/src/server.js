19b039017461754b364039b9726a899807ecc290
Breno Nascimento <satsukebare23@gmail.com>
2022-10-01 10:20:41 -0300

    config server

--- backend/trybecash-api/re-config/src/server.js
+++ backend/trybecash-api/re-config/src/server.js
@@ -1,1 +1,7 @@
-const express = require(express)
+const app = require('./app');
+
+const port = 3001;
+
+app.listen(port, async () => {
+  console.log(`API TrybeCash sendo executada na porta ${port}`);
+});
